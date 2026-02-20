3a2b5d259388f3adc9b6934a6bfc1fee3c575630
Evgenii Khe <kheeugene@gmail.com>
2026-02-20 13:26:17 -0500

    Initial commit

---
+++ src/main/java/org/mailatlas/mailatlas/MailatlasApplication.java
@@ -0,0 +1,13 @@
+package org.mailatlas.mailatlas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MailatlasApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MailatlasApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/mailatlas/mailatlas/MailatlasApplicationTests.java
@@ -0,0 +1,13 @@
+package org.mailatlas.mailatlas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MailatlasApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
