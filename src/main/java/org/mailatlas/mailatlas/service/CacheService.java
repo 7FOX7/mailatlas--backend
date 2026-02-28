d989da52d0e1d172d418372dc05262dc43a57075
Evgenii Khe <kheeugene@gmail.com>
2026-02-28 17:48:30 -0500

    Various improvements to cache service

--- src/main/java/org/mailatlas/mailatlas/service/CacheService.java
+++ src/main/java/org/mailatlas/mailatlas/service/CacheService.java
@@ -1,29 +1,48 @@
 package org.mailatlas.mailatlas.service;
 
 import jakarta.validation.constraints.Email;
+import lombok.extern.slf4j.Slf4j;
 import org.mailatlas.mailatlas.entity.UserData;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.stereotype.Service;
+import tools.jackson.databind.ObjectMapper;
 
 import java.util.concurrent.TimeUnit;
 
+@Slf4j
 @Service
 public class CacheService {
     // inject redis
     @Autowired
     private RedisTemplate<String, Object> cache;
+    private final Integer DEFAULT_TTL = 30;
+    // provide a default message
+    private StringBuilder messageBuilder = new StringBuilder("(Redis): ");
 
     public UserData getData(@Email String email) {
-        // check if there's a user data in cache
-        return (UserData) cache.opsForValue().get(email);
+        if (!cache.hasKey(email)) {
+            String message = messageBuilder.append("key " + email + " doesn't exist. Make sure you're adding it.").toString();
+            log.warn(message);
+            return null;
+        }
+
+        Object cached = cache.opsForHash().get(email, email);
+        // use object mapper to convert cached data to user data
+        return (new ObjectMapper()).convertValue(cached, UserData.class);
     }
 
     public void setData(UserData userData) {
         // extract email
         @Email String email = userData.getEmail();
-        cache.opsForSet().add(email, userData);
+
+        if (cache.hasKey(email)) {
+            String message = messageBuilder.append("key " + email + " already existed for some reason. Deletion will be performed first before putting email.").toString();
+            log.warn(message);
+            cache.delete(email);
+        }
+        cache.opsForHash().put(email, email, userData);
         // set ttl
-        cache.expire(email, 60, TimeUnit.DAYS);
+        cache.expire(email, DEFAULT_TTL, TimeUnit.DAYS);
     }
 }
